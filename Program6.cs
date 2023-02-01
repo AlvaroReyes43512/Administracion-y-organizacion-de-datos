fc701181c01da17b3c2a5d338ddc928286ac6a29
AlvaroReyes43512 <112416760+AlvaroReyes43512@users.noreply.github.com>
2023-01-31 19:24:24 -0600

    Programa 6

---
+++ Program6.cs
@@ -0,0 +1,18 @@
+﻿//Hacer un programa que lea un numero, si es el numero no es par. se volvera a pedir, de lo contrario mostrara todos
+//los numero que hay entre el 0 y el 
+//Alvaro Dannte Reyes Mercado
+int a;
+int x;
+do
+{
+Console.WriteLine("Ingrese un numero");
+a = Convert.ToInt32(Console.ReadLine());
+
+} while ((a%2==1)||(a<=0));
+{
+    Console.WriteLine("Los datos son");
+    for (x=0; x <= a; x++)
+    {
+        Console.WriteLine(x);
+    }
+}
